d6a6bc37137fdcda68d5bb07febb7a1e59ae6a0d
suinautant <suinautant@gmail.com>
2021-12-08 21:56:26 +0900

    add login/join page

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // 가입
 router.get('/join', (req, res, next) => {
+    res.render('user/join', { title: 'join' });
+});
+
+router.post('/join', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log('username : ' + username);
+    console.log('password : ' + password);
+
+    if (username !== 'admin') {
+        res.send('admin의 가입만 가능합니다.');
+        res.end();
+    }
+
     res.render('user/join', { title: 'join' });
 });
 
--- views/js/user/join_checkId.js
+++
@@ -1,1 +0,0 @@
-console.log('join_checkid');
