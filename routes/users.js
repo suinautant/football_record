69e80999e33ceaff68fee48a986dd174348f04ce
suinautant <suinautant@gmail.com>
2021-12-09 20:19:37 +0900

    dbconfig

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config/dbconfig');
+// ../config/dbconfig
+// host: 'localhost',
+// user: 'username',
+// password: 'password',
+// database: 'fbrecord',
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+});
+
+connection.connect();
+
+connection.query('SELECT * from users', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].username);
+});
+
+connection.end();
 
 // 가입
 router.get('/join', (req, res, next) => {
