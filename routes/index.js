ec9107a446c1932e20a3a3c04fc3705c6b535b79
suinautant <suinautant@gmail.com>
2021-12-20 19:02:10 +0900

    test 삭제 : dotenv 사용 : db config file 삭제

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    console.log(process.env.TEST);
     res.render('index', { title: 'Express' });
 });
 
