4f4db5e2a0f4dd4f559200a967ec0826ed0007f5
suinautant <suinautant@gmail.com>
2022-01-07 16:52:49 +0900

    기본 디자인

--- routes/match.js
+++ routes/match.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const rawDatabase = {
-    otherTeam: '경주한수원',
-    date: '2020-2-3 19:00',
-    location: '김해운동장',
-    points: 3,
-    goalsFor: 2,
-    goalsAgainst: 1,
-    home: true,
+  otherTeam: '경주한수원',
+  date: '2020-2-3 19:00',
+  location: '김해운동장',
+  points: 3,
+  goalsFor: 2,
+  goalsAgainst: 1,
+  home: true,
 };
 
 router.get('/', (req, res, next) => {
-    console.log(rawDatabase);
-    res.render('match', { title: 'match', data: rawDatabase });
+  console.log(rawDatabase);
+  res.render('match', { title: 'match', data: rawDatabase });
 });
 
 module.exports = router;
