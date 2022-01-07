0a68020e15467638dad52023193d147dd7adb2f6
suinautant <suinautant@gmail.com>
2022-01-07 15:41:58 +0900

    FEAT: add 경기기록 페이지 메뉴/라우터/템플릿

--- routes/match.js
+++ routes/match.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// PR 로그
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('match', { title: 'match' });
