5f9af7c8d38fc3d0d8db77ab322fce599970b5b3
suinautant <suinautant@gmail.com>
2022-01-11 19:09:46 +0900

    issues#5 Refactor: clean code

--- routes/match.js
+++ routes/match.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const rawDatabase = {
-  otherTeam: '경주한수원',
-  date: '2020-2-3 19:00',
-  location: '김해운동장',
-  points: 3,
-  goalsFor: 2,
-  goalsAgainst: 1,
-  home: true,
-};
-
 router.get('/', (req, res, next) => {
-  console.log('/match/');
-  console.log(rawDatabase);
-  res.render('match', { title: 'match', data: rawDatabase });
+  res.render('match');
 });
 
 module.exports = router;
