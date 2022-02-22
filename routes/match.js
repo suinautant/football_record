1da9a59fc54c7e35e08854f93572b7cc2f206bd7
suinautant <suinautant@gmail.com>
2022-02-22 15:03:25 +0900

    ADD: match/id router

--- routes/match.js
+++ routes/match.js
@@ -5,4 +5,8 @@ router.get('/', (req, res, next) => {
   res.render('match');
 });
 
+router.get('/:id', (req, res, next) => {
+  res.render('match_detail', { id: req.params.id });
+});
+
 module.exports = router;
